2db39239d3ae63734f9b1ca310e9f832434db909
Ines Chakroun <ineschakrouwn@gmail.com>
2023-12-21 12:45:27 +0100

    wip

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -32,7 +32,7 @@ async function displayProject() {
       article.dataset.id = "3";
     }
     articleTitle.textContent = element.title;
-    Parent = document.getElementById("MyProjects");
+    const Parent = document.getElementById("MyProjects");
     Parent.appendChild(article);
     article.appendChild(articleImage);
     article.appendChild(articleTitle);
@@ -41,29 +41,34 @@ async function displayProject() {
 displayProject();
 
 // Création des filtres
-function Filtres(innerHTML, className, id) {
+function filtres(innerHTML, className, id) {
   let filter = document.createElement("button");
   filter.innerHTML = innerHTML;
   filter.className = className;
   filter.id = id;
-  const container = document.getElementById("filtres");
+  const container = document.getElementById("les_Filtres");
   container.appendChild(filter);
+  return container;
 }
 
-Filtres("Tous", "all actif", "0");
-Filtres("Objets", "objects inactif", "1");
-Filtres("Appartements", "appartments inactif", "2");
-Filtres("Hôtels & restaurants", "Hôtels_restaurants inactif", "3");
+filtres("Tous", "classe_filtre actif", "0");
+filtres("Objets", "classe_filtre inactif", "1");
+filtres("Appartements", "classe_filtre inactif", "2");
+filtres("Hôtels & restaurants", "classe_filtre inactif", "3");
 
 // Ajout des EventListeners pour les filtres
-let filters = document.querySelectorAll("#filtres button");
+let filters = document.querySelectorAll("#les_Filtres button");
 
 for (let filter of filters) {
-  filter.addEventListener("click", function () {
-    let tag = this.id;
+  filter.addEventListener("click", function (e) {
+    tag = this.id;
+    if (document.querySelector("#les_Filtres button.actif") !== null) {
+      document
+        .querySelector("#les_Filtres button.actif")
+        .classList.replace("actif", "inactif");
+    }
+    e.target.className = "classe_filtre actif";
 
-    filters[tag].classList.replace("inactif", "actif");
-    filters[tag - 1].classList.replace("actif", "inactif");
     let articles = document.querySelectorAll("#MyProjects figure");
     for (let article of articles) {
       article.classList.replace("active", "inactive");
@@ -75,20 +80,23 @@ for (let filter of filters) {
 }
 
 // Création de la page de connexion
-const int = document.getElementById("introduction");
+/*const int = document.getElementById("introduction");
 const port = document.getElementById("portfolio");
 const cont = document.getElementById("contact");
-const main = document.getElementById("Main");
+const main = document.getElementById("Main");*/
+
 const Bouton_Login = document.getElementById("Bouton");
 
-Bouton_Login.addEventListener(
-  "click",
-  function () {
-    Bouton_Login.className = "login";
+Bouton_Login.addEventListener("click", function () {
+  location.href = "./login.html";
+});
+
+/*Bouton_Login.className = "login";
     int.className = "off";
     port.className = "off";
     cont.className = "off";
     const conteneur = document.createElement("section");
+    conteneur.id = "Formulaire";
     const titre = document.createElement("h2");
     titre.innerHTML = "Log In";
     titre.className = "h2";
@@ -102,40 +110,35 @@ Bouton_Login.addEventListener(
     conteneur.appendChild(paragraphe);
     main.appendChild(conteneur);
     main.className = "mainClass";
-    const logo= document.getElementById("logo")
-    logo.classList="curseur"
-    logo.addEventListener("click",function(){
-      window.location="./index.html"
-    })
-   
+    const logo = document.getElementById("logo");
+    logo.classList = "curseur";
+    logo.addEventListener("click", function () {
+      window.location = "./index.html";
+    });
   },
   { once: true }
-);
+);*/
 
 const form = document.getElementById("formulaire");
 
 form.addEventListener("submit", (Event) => {
   Event.preventDefault();
+  //let logged = false;
   let user = {
     email: document.getElementById("e_mail").value,
     password: document.getElementById("mot de passe").value,
   };
 
-  PostUser();
-  // Création de la charge utile au format JSON
-
-  async function PostUser() {
+  async function postUser() {
     const response = await fetch("http://localhost:5678/api/users/login", {
       method: "POST",
       headers: { "Content-Type": "application/json;charset=utf-8" },
       body: JSON.stringify(user),
     });
-      result = await response.json();
-      
-   
-     
+    result = await response.json();
+
     if (response.status == 404 || response.status == 401) {
-      console.log(result)
+      console.log(result);
       const erreur_message = document.createElement("span");
       erreur_message.innerHTML = " ERROR : " + result.message;
       erreur_message.className = "alert";
@@ -144,14 +147,36 @@ form.addEventListener("submit", (Event) => {
       //alert("Erreur !")
     }
     if (response.status == 200) {
-       console.log(result)
-      window.localStorage.setItem("token",result.token)
-      const token=window.localStorage.getItem("token")
-      console.log(token)
-      
-      
-      
-      window.location = "./index1.html";
+      window.localStorage.setItem("token", result.token);
+      const token = window.localStorage.getItem("token");
+      console.log(token);
+      logged = true;
+    }
+    return logged;
+  }
+
+  async function redirection() {
+    const feedback = await postUser();
+    console.log(feedback);
+    if (feedback == true) {
+      window.location = "./index1.html"; //"index.html"
+      console.log(document);
     }
   }
+  redirection();
+
+  //je ne peux pas accéder au contenu de "index1.html" !!
+  /*const modal= document.getElementById("bouton_modal")
+    console.log(modal)
+     modal.addEventListener("click", async function(){
+    
+      const project = await getProjets();
+      console.log(project)
+    
+    })*/
+  /* const les_Filtres=document.getElementById("les_Filtres")
+    console.log(les_Filtres) //toujours pas d'accès au contenu !!
+  }}
+     
+    redirection()*/
 });
