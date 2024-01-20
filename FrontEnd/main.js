ff2e844bed08420c78524891953407c27729c9ba
Ines Chakroun <ineschakrouwn@gmail.com>
2024-01-20 18:44:49 +0100

    refactoring

---
+++ FrontEnd/main.js
@@ -0,0 +1,24 @@
+// Récupérer les projets de l'architecte 
+import {projects } from "./projects.js";
+projects();
+
+// Création des filtres
+import { filtres } from "./filters.js";
+filtres("Tous", "classe_filtre actif", "0");
+filtres("Objets", "classe_filtre inactif", "1");
+filtres("Appartements", "classe_filtre inactif", "2");
+filtres("Hôtels & restaurants", "classe_filtre inactif", "3");
+
+//Page de connexion
+const Bouton_Login = document.getElementById("Bouton");
+Bouton_Login.addEventListener("click", function () {
+  location.href = "./login.html";
+});
+
+// Formulaire de connexion & Redirection
+import { form } from "./form.js";
+form();
+
+// Ajout de la modale
+import { modal } from "./modal.js";
+modal();
